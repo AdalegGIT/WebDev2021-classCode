a55f67b4e9537874bca4559e1f641416e61a4919
harisree06 <harisree.n.s@gmail.com>
2021-02-04 21:16:31 -0500

    adding activerecord methods

--- Topic10::Intro_toSQL/film.rb
+++ Topic10::Intro_toSQL/film.rb
@@ -1,28 +1,66 @@
-class Film
-    attr_accessor :id, :title, :country, :genre, :rating, :description
-  
-    @@library = []
-    @@id = 1;
-  
-    def initialize(options)
-      @title = options[:title]
-      @country = options[:country]
-      @genre = options[:genre]
-      @rating = options[:rating]
-      @description = options[:description]
-      @id = @@id
-  
-      @@id += 1
-    end
-  
-    def self.create(options)
-      film = self.new(options)
-      @@library << film
-      return film
-    end
-  
-    def self.all
-      return @@library
-    end
-  
-  end
+gem 'activerecord', '=4.2.10' #specify this if you are not using the latest version ( the whole line)
+require 'active_record'
+require 'mini_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'films.sqlite3') #establish the connection
+
+class Film < ActiveRecord::Base
+       field :title, as: :string #@title --> method call
+       field :country, as: :string #@country
+       field :genre, as: :string #@genre
+       field :rating, as: :integer #@rating
+end
+
+Film.auto_upgrade!
+
+# a = Film.create(title: "Star Wars",country: "USA")
+# p a
+# Film.create(title: "Into the Wild",country: "USA", rating: 5)
+#CRUD
+#CREATE
+my_obj = Film.new(title: "Batman", country:"USA")
+#this object is not yet saved to the DB
+
+my_obj.rating=4
+
+#this object is still not saved in the db
+
+my_obj.save
+#it gets saved to the DB
+
+
+#option2
+my_obj1 = Film.create(title: "new film")
+
+#READ
+Film.all #--> returns all the objects as saved in the DB in an array
+Film.find(3) #--> gives the film with ID 
+Film.find_by(title: "Matrix") #--> returns the first film that matches the condition
+Film.where(title: "Star Wars") #--> returns an array of films with the matching title
+Film.first
+Film.last 
+
+#UPDATE
+#find the film to update
+film_to_update = Film.find(4)
+
+#option1
+film_to_update.genre = "Sci-fi"
+film_to_update.save
+
+#option2
+film_to_update.update(country: "USA" , rating: 4)
+
+
+#DELETE
+film_to_delete = Film.find_by(title: "Super Man")
+film_to_delete.destroy #--> Instance method to delete
+
+Film.destroy_by(title: "Star Wars") #-->*special case class method destroys all the objects with matching condition
+
+Film.destroy_all #--> removes all the records from the DB
+
+
+
+
+
