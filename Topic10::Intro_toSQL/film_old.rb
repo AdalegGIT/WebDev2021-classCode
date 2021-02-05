df8f67283bb2e948e0a71468c5d90b9db052ac75
harisree06 <harisree.n.s@gmail.com>
2021-02-04 21:28:17 -0500

    adding old version of film class

---
+++ Topic10::Intro_toSQL/film_old.rb
@@ -0,0 +1,34 @@
+class Film
+    attr_accessor :id, :title, :country, :genre, :rating, :description
+  
+    @@library = []
+    @@id = 1;
+  
+    def initialize(options)
+      @title = options[:title]
+      @country = options[:country]
+      @genre = options[:genre]
+      @rating = options[:rating]
+      @description = options[:description]
+      @id = @@id
+  
+      @@id += 1
+    end
+  
+    def self.create(options)
+      film = self.new(options)
+      @@library << film
+      return film
+    end
+  
+    def self.all
+      return @@library
+    end
+
+    def delete
+        @@library.delete(self)
+    end
+  
+  end
+
+  Film.create(title: "Star Wars")
