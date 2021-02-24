b07494bf9ad3e21d129a292a1051ff03a8e83a0a
Peter, Elvis <Elvis.Peter@bmo.com>
2021-02-23 20:55:33 -0500

    post req

--- Topic12::Sinatra/server.rb
+++ Topic12::Sinatra/server.rb
@@ -1,7 +1,15 @@
 require 'sinatra'
 
+#defining local variables 
 name = "Elvis Peter"
 email = "elvispeter91@gmail.com"
+todo = [
+    'Water the plants',
+    'Feed the cat',
+    'Call the girlfriend',
+    'Cook the dinner',
+    'Blah blah blah' 
+  ];
 
 get '/' do
     "<h1>hello world</h1>"
@@ -14,10 +22,28 @@ end
 
 get '/contactMe' do
     @name = name
+
     @el = email
     erb :contact_me
 end
 
 get '/msg' do
-    "<h1>People r gud :-)</h1>"
-end`
+    redirect to('/helloPeople')
+end
+
+get '/todo' do
+   @todoList = todo
+    erb :todo
+end
+
+get '/createTask' do
+    erb :create_task
+end
+
+post '/createNewTodo' do
+    # p "hello am in a post request finally!!"
+    # p "==== #{params["todo"]}"
+    new_item = params["todo"]
+    todo.push(new_item)
+    redirect to('/todo')
+end
